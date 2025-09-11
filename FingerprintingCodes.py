7b27441438cf30da59b76def2c914f97626b0042
david <david.brand@stud.tu-darmstadt.de>
2025-09-11 16:11:09 +0200

    added Fingerprinting Codes of logarithmic length

--- FingerprintingCodes.py
+++ FingerprintingCodes.py
@@ -7,7 +7,7 @@ class FingerprintingCode:
 
     def __init__(self, n, e, d=0):
         if d == 0:
-            self.d = math.floor(2 * math.pow(n, 2) * math.log((2 * n) / e))
+            self.d = math.ceil(2 * math.pow(n, 2) * math.log((2 * n) / e))
         else:
             self.d = d
         self.n = n
@@ -61,7 +61,59 @@ class FingerprintingCode:
         return c
 
 
+class LogLengthCodes:
+
+    def __init__(self, N, c, e):
+        self.N = N
+        self.L = math.ceil(2 * c * math.log((2 * N) / e))
+        self.c = c
+        self.n = 2 * c
+        self.e = e
+        self.d = math.ceil(2 * math.pow(self.n, 2) * math.log((4 * self.n * self.L) / e))
+        self.codes = [FingerprintingCode(self.n, e, self.d) for i in range(self.L)]
+        self.hiddenCode = self.createHiddenCode()
+        self.codeBook = self.writeCodeBook()
+
+    def createHiddenCode(self):
+        h = []
+        for i in range(self.N):
+            h.append([random.randint(0, self.n - 1) for j in range(self.L)])
+        return h
+
+    def writeCodeBook(self):
+        cb = []
+        for h in self.hiddenCode:
+            tmp = BitArray()
+            for i in range(self.L):
+                tmp.append(self.codes[i].codeBook[h[i]])
+            cb.append(tmp)
+        return cb
+
+    def trace(self, x):
+        l = self.d*(self.n-1)
+        y = []
+        for i in range(self.L):
+            y.append(self.codes[i].trace(BitArray(bin=x.bin[(i*l):((i+1)*l)]))[0])
+        index = 0
+        matches = 0
+        for i in range(self.N):
+            count = 0
+            for j in range(self.L):
+                if self.hiddenCode[i][j] == y[j]:
+                    count += 1
+            if count > matches:
+                index = i
+                matches = count
+        return index
+
+    def collude(self, i, j):
+        l = self.L * self.d * (self.n-1)
+        c = BitArray(l)
+        for k in range(l):
+            c[k] = (self.codeBook[i][k] == 1 & self.codeBook[j][k] == 1)
+        return c
+
+
 if __name__ == '__main__':
-    a = FingerprintingCode(4, 0.2)
-    a.out()
-    print(a.trace(a.collude(1, 2)))
+    a = LogLengthCodes(10, 2, 0.1)
+    print(a.trace(a.collude(2, 4)))
